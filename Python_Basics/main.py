c6638eb8cd2c9b6c7ed748810540d93cdb719de4
ViniPallakki <vinayakapallakki@gmail.com>
2026-05-15 22:19:44 +0530

    lastest code

--- Python_Basics/main.py
+++ Python_Basics/main.py
@@ -496,4 +496,116 @@ import string
 #     a=1
 
 
+#             0      1            2   
+# mylist = ["apple", "banana", "cherry"]
+# print(mylist[0])
 
+#collection of items in a particular order
+#consists of []
+#list can contain different data types
+#list is mutable, meaning you can change its content
+#list allows duplicate values
+
+# thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist[1])
+# print(thislist[-1])
+#          -7        -6       -5       -4          -3         -2       -1
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[-4:])
+
+# print(thislist[:4])
+# print(thislist[2:])
+# ['orange', 'kiwi', 'melon']
+
+# print(thislist[2:5])
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1] = "blackcurrant"
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# thislist[1:3] = ["blackcurrant", "watermelon"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist)
+# # thislist.insert(2, "watermelon")
+# thislist.insert(10, "watermelon")
+# print(thislist[10])
+# print(thislist) 
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.append("orange")
+# print(thislist)
+# thislist.insert(1, "orange")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# tropical = ["mango", "pineapple", "papaya"]
+
+#['apple', 'banana', 'cherry', 'mango', 'pineapple', 'papaya']
+# thislist.extend(tropical)
+# print(thislist)
+# print(thislist + tropical)
+#######################assignnment############################
+# thislist = ["apple", "banana", "cherry"]
+# thistuple = ("kiwi", "orange")
+# print(thislist + thistuple)
+# print(thislist + list(thistuple))
+# print(thislist.extend(thistuple))
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.remove("banana")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# # thislist.pop(1)
+# thislist.pop(-1)
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# # thislist.clear()
+# # print(thislist)
+
+# for x in thislist:
+#   print(x)
+
+# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# thislist.sort()
+# print(thislist)
+
+# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# thislist.sort(reverse = True)
+# print(thislist)
+
+# thislist = [100, 50, 65, 82, 23]
+
+# thislist.sort()
+
+# print(thislist)
+
+# mylist = [1, "apple", 3, "banana", 5]
+# mylist.sort()  # This will raise a TypeError because you cannot compare integers and strings for sorting.
+# print(mylist)
+
+# thislist = ["apple", "bana", "cherry"]
+# print(thislist)
+# mylist = thislist.copy()
+# print(mylist)
+
+
+# append()	Adds an element at the end of the list
+# clear()	Removes all the elements from the list
+# copy()	Returns a copy of the list
+# count()	Returns the number of elements with the specified value
+# extend()	Add the elements of a list (or any iterable), to the end of the current list
+# index()	Returns the index of the first element with the specified value
+# insert()	Adds an element at the specified position
+# pop()	Removes the element at the specified position
+# remove()	Removes the item with the specified value
+# reverse()	Reverses the order of the list
+# sort()	Sorts the list
